b8f0fdedab4d658d382b11c4796ff7d5639654cc
haniokasai@Desktop <htek@haniokasai.com>
2019-10-27 18:07:24 +0900

    apt clean

--- aptinstaller.sh
+++ aptinstaller.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 sed -i.bak -e "s%http://[^ ]\+%http://ftp.iij.ad.jp/pub/linux/ubuntu/archive/%g" /etc/apt/sources.list
 apt update
-apt install apt-transport-https ca-certificates  curl  gnupg-agent software-properties-common  unzip zip openjdk-8-jre -y
+apt install apt-transport-https ca-certificates  curl  gnupg-agent software-properties-common -y
+apt clean
+apt install openjdk-8-jre -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
