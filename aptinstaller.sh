87eb6828e89c58477f7bda1d27258f95a77283f5
haniokasai@Desktop <htek@haniokasai.com>
2019-10-27 18:23:55 +0900

    remvoe sudo

--- aptinstaller.sh
+++ aptinstaller.sh
@@ -4,7 +4,7 @@ apt update
 apt install apt-transport-https ca-certificates  curl  gnupg-agent software-properties-common -y
 apt clean
 apt install openjdk-8-jre -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
